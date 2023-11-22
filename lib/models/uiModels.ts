7ebf344606242e527d6c4ffd2bddcd10cd882daf
Vegetable Destroyer <dreamsky0701@hotmail.com>
2023-11-22 21:03:45 +0200

    feat: update notification model

--- lib/models/uiModels.ts
+++ lib/models/uiModels.ts
@@ -11,3 +11,16 @@ export interface Buttons {
   onAddInvoice?: (state: boolean) => void;
   inputClick?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+export interface Notification {
+  id: string;
+  content?: string;
+  icon?: 'success' | 'error' | 'trash' | 'save';
+  autoHideDuration?: number;
+}
+
+export interface NotificationContainer {
+  messages: Array<Notification>;
+  defaultDuration?: number;
+  gap?: number;
+}
