3eb9905c16f47bfa5c512ae09bd725b07cc1309e
Vegetable Destroyer <dreamsky0701@hotmail.com>
2023-12-24 02:52:12 +0200

    feat: update codebase

--- components/Projects/ProjectButtons.tsx
+++ components/Projects/ProjectButtons.tsx
@@ -158,6 +158,7 @@ const ProjectButtons = (props: Props) => {
   };
 
   const handleBuildB2AReport = async (clientBillIds: string[]) => {
+    setOpenConfirmModal(false);
     if (!clientBillId || !clientBillIds || !clientBillIds.length) return;
     dispatch(
       uiActions.setTaskLoadingState({
