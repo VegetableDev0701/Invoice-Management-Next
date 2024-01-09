5391c281c02543cdb9f0e0b7ba6da1f6ec726e8a
Vegetable Destroyer <dreamsky0701@hotmail.com>
2024-01-09 10:50:36 +0200

    style: format code

--- components/Tables/Invoices/InvoiceSortHeadingsTable.tsx
+++ components/Tables/Invoices/InvoiceSortHeadingsTable.tsx
@@ -470,8 +470,10 @@ const InvoicesTable = <H extends InvoiceTableHeadings>(props: Props<H>) => {
                                     setSelected(
                                       isChecked
                                         ? [...selected, invoice]
-                                        : selected.filter((el) => el !== invoice)
-                                    )
+                                        : selected.filter(
+                                            (el) => el !== invoice
+                                          )
+                                    );
                                   }
                                   setLastIndex(index);
                                 }}
